daa223ce3cd605e16e1d120c28d318824d0a93f3
joshleetufts <josh@cs.tufts.edu>
2017-05-30 16:30:57 -0400

    fix only md files showing 2

--- compose.rb
+++ compose.rb
@@ -184,7 +184,7 @@ t = Time.new
 tstamp = t.strftime("%Y-%m-%d %H:%M:%S")
 dstamp = t.strftime("%Y-%m-%d")
 
-ext = cfgfile['compose']['extension'] ? cfgfile['compose']['extension'] : 'md'
+ext = cfgfile['compose']['extension'] ? cfgfile['compose']['extension'] : 'html'
 
 fname = dstamp + "-" + tslug + '.' + ext
 
